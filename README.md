e255d91cf03e3bb5aa8ac125fe57d5565aaf00e6
progfay <19568747+progfay@users.noreply.github.com>
2025-07-10 23:04:27 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# progfay/hatsukey70
+
+You can [build firmware](https://remap-keys.app/catalog/oycLWcG0WHRrSohYAmiY/build) and [flush keymap](https://remap-keys.app/configure) on [REMAP](https://remap-keys.app/).
+
---
+++ config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define MOUSEKEY_DELAY 100
+#define MOUSEKEY_INTERVAL 5
+#define MOUSEKEY_MAX_SPEED 5
+#define MOUSEKEY_TIME_TO_MAX 80
+
+#define MK_3_SPEED
+#define MK_C_OFFSET_0 1 /* pixels */
+#define MK_C_INTERVAL_0 30 /* milliseconds */
+#define MK_W_OFFSET_0 1
+#define MK_W_INTERVAL_0 120
+#define MK_C_OFFSET_1 12
+#define MK_C_INTERVAL_1 5
+#define MK_W_OFFSET_1 1
+#define MK_W_INTERVAL_1 80
+#define MK_C_OFFSET_2 15
+#define MK_C_INTERVAL_2 3
+#define MK_W_OFFSET_2 1
+#define MK_W_INTERVAL_2 40
