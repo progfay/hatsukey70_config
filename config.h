34e1fd6299c46dbca1552a33eaf1b1f47ff1f4cf
Shunsuke Mano <19568747+progfay@users.noreply.github.com>
2026-04-04 16:57:20 +0900

    Refactor mouse key settings in config.h
    Updated mouse key configuration parameters for improved performance.

--- config.h
+++ config.h
@@ -4,21 +4,16 @@
 #define PERMISSIVE_HOLD
 #define RETRO_TAPPING
 
-#define MOUSEKEY_DELAY 100
-#define MOUSEKEY_INTERVAL 5
-#define MOUSEKEY_MAX_SPEED 5
-#define MOUSEKEY_TIME_TO_MAX 80
-
 #define MK_3_SPEED
-#define MK_C_OFFSET_0 1 /* pixels */
-#define MK_C_INTERVAL_0 30 /* milliseconds */
+#define MK_C_OFFSET_0 12
+#define MK_C_INTERVAL_0 5
 #define MK_W_OFFSET_0 1
-#define MK_W_INTERVAL_0 120
-#define MK_C_OFFSET_1 12
-#define MK_C_INTERVAL_1 5
+#define MK_W_INTERVAL_0 80
+#define MK_C_OFFSET_1 15
+#define MK_C_INTERVAL_1 3
 #define MK_W_OFFSET_1 1
-#define MK_W_INTERVAL_1 80
+#define MK_W_INTERVAL_1 50
 #define MK_C_OFFSET_2 15
-#define MK_C_INTERVAL_2 3
+#define MK_C_INTERVAL_2 2
 #define MK_W_OFFSET_2 1
 #define MK_W_INTERVAL_2 40
